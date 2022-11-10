60fb2e44fb110fb4de7ac626764ce3defc08ddb7
to <t.higashimoto.sys22@morijyobi.ac.jp>
2022-11-10 11:56:59 +0900

    訂正済み

--- Main.java
+++ Main.java
@@ -12,6 +12,19 @@ public class Main {
                 double bmi = weight / (Math.pow(height, 2) * 100) * 100;
 
                 System.out.println("BMI値は"+ (Math.floor(bmi * 100)) / 100);
+
+
+		Scanner stdIn2 = new Scanner(System.in);
+		System.out.print("身長:");
+                double height2 = stdIn2.nextDouble();
+                System.out.print("体重:");
+                double weight2 = stdIn2.nextDouble();
+
+                double bmi2 = weight2 / (Math.pow(height2, 2) * 100) * 100;
+
+                System.out.println("BMI値は"+ (Math.floor(bmi2 * 100)) / 100);
+
+
         }
 }
 
